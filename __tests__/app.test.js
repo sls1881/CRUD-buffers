bd268bebe5ae808afd08cc4cad411d85ec62b65d
‘Shelby <sls1881@gmail.com>
2021-04-01 12:06:44 -0700

    post and get route completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,8 +4,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 const Student = require('../lib/models/Student');
 
-jest.mock('../lib/utils/twilio.js');
+//This needs to be import before the mock
 const twilio = require('../lib/utils/twilio.js'); 
+jest.mock('../lib/utils/twilio.js');
 
 describe('buffer routes', () => {
   beforeEach(() => {
@@ -18,7 +19,8 @@ describe('buffer routes', () => {
 
     twilio.sendSms.mockClear();
   })
-
+ 
+ //Post test
   it('should post a new student to the database and send a welcome text message', () => {
     return request(app)
     .post('/api/v1/students')
@@ -27,4 +29,42 @@ describe('buffer routes', () => {
       expect(twilio.sendSms).toHaveBeenCalledTimes(1);
     })
   })
+
+  //GET all students test
+  it('should get all students from the database', () => {
+    return request(app)
+    .get('/api/v1/students')
+    .then((students) => {
+      expect(students.body).toEqual([student]);
+    })
+  })
+
+  //GET student by ID test
+  it('should get a student from the database by ID', () => {
+    return request(app)
+    .get('/api/v/students/${student.id}')
+    .then((studentId) => {
+      expect(studentId.body).toEqual([student]);
+    })
+  })
+
+  // //Update student by ID
+  // it('should post a new student to the database and send a welcome text message', () => {
+  //   return request(app)
+  //   .put('/api/v1/students')
+  //   .send({studentName: 'Shelby Strouse', grade: 'A'})
+  //   .then(() => {
+  //     expect(twilio.sendSms).toHaveBeenCalledTimes(1);
+  //   })
+  // })
+
+  // //Delete student by ID
+  // it('should post a new student to the database and send a welcome text message', () => {
+  //   return request(app)
+  //   .delete('/api/v1/students')
+  //   .send({studentName: 'Shelby Strouse', grade: 'A'})
+  //   .then(() => {
+  //     expect(twilio.sendSms).toHaveBeenCalledTimes(1);
+  //   })
+  // })
 });
